ebcfe316e3f82a04f19bfa9b4dec963063de48c4
nikitooooos <nikita.postnikov.00@gmail.com>
2023-09-06 11:01:56 +0300

    change web page

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -8,7 +8,7 @@ public class SearchTests {
     @Test
     void successfulSearchTest() {
         open("https://www.google.com/");
-        $("[name=q]").setValue("selenide").pressEnter();
-        $("[id=search]").shouldHave(text("https://selenide.org"));
+        $("[name=q]").setValue("vk").pressEnter();
+        $("[id=search]").shouldHave(text("https://m.vk.com"));
     }
 }
