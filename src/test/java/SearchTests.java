e46d9f0fe8e0c2cd76acd9b3d5ce66650be22be7
nikitooooos <nikita.postnikov.00@gmail.com>
2023-09-06 10:56:36 +0300

    commit change web-page

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -8,7 +8,7 @@ public class SearchTests {
     @Test
     void successfulSearchTest() {
         open("https://www.google.com/");
-        $("[name=q]").setValue("selenide").pressEnter();
-        $("[id=search]").shouldHave(text("https://selenide.org"));
+        $("[name=q]").setValue("yandex").pressEnter();
+        $("[id=search]").shouldHave(text("https://ya.ru"));
     }
 }
